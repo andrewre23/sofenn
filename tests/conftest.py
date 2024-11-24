55f04565b5ac3f0e350ed34a15f78df622bc438d
Andrew Edmonds <andrewre23@gmail.com>
2024-11-24 09:44:03 -0500

    added conftest.py to set random seed across testing

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def set_random_seed():
+    # Seed value
+    # Apparently you may use different seed values at each stage
+    seed_value = 0
+
+    # 1. Set the `PYTHONHASHSEED` environment variable at a fixed value
+    import os
+    os.environ['PYTHONHASHSEED'] = str(seed_value)
+
+    # 2. Set the `python` built-in pseudo-random generator at a fixed value
+    import random
+    random.seed(seed_value)
+
+    # 3. Set the `numpy` pseudo-random generator at a fixed value
+    import numpy as np
+    np.random.seed(seed_value)
+
+    # 4. Set the `tensorflow` pseudo-random generator at a fixed value
+    import tensorflow as tf
+    #tf.random.set_seed(seed_value)
+    # for later versions:
+    tf.compat.v1.set_random_seed(seed_value)
+
+    # 5. Configure a new global `tensorflow` session
+    from keras import backend as K
+    session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+    sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+    K.set_session(sess)
+    # for later versions:
+    # session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+    # sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph(), config=session_conf)
+    # tf.compat.v1.keras.backend.set_session(sess)
