33518fe01b77f33eb5850e7c43bb13858cc556fc
Andrew Edmonds <andrewre23@gmail.com>
2025-07-02 04:16:25 +0000

    Version bump to v0.1.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ build_requirements = read_requirements('build-requirements.txt')
 
 setup(
     name='sofenn',
-    version='0.1.4',
+    version='0.1.5',
     author='Andrew Edmonds',
     author_email='andrewre23@gmail.com',
     description='Keras model of a Self-Organizing Fuzzy Network',
